0955ee62e96f469da504c2409f4c789de4073bc7
thiwnthah <phamngocthienthanh207@gmail.com>
2025-06-22 08:20:00 +0700

    Update danhsach.js

--- danhsach.js
+++ danhsach.js
@@ -65,11 +65,11 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
+        document.title = "PAGE | THIWN THAH";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
-        document.title = "Come Back To Portfolio";
+        document.title = "WEB | THIWN THAH";
         $("#favicon").attr("href","/assets/images/favhand.png");
     }
 });
